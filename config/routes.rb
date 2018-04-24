eb8add07c1835e58f079a3f0c67bff9072f38549
Naoki Shinagawa <naoki.shina@gmail.com>
2018-04-25 01:04:05 +0900

    add root n adjustment css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/top'
   get 'home/about'
-
+  root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
