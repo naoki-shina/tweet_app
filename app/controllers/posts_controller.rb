7535d95d238a8f48873d168548871026760e32ab
Naoki Shinagawa <naoki.shina@gmail.com>
2018-04-25 19:09:34 +0900

    add edit action for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :DESC)
   end
 
   def new
@@ -14,5 +14,8 @@ class PostsController < ApplicationController
 
   def show
      @post = Post.find_by(id: params[:id])
+  end
+
+  def edit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get '/posts/index'
-  get '/posts/new', to: 'posts#new'
-  post '/posts/create', to: 'posts#create'
-  get '/posts/:id', to: 'posts#show'
+  get 'posts/index'
+  get 'posts/new', to: 'posts#new'
+  post 'posts/create', to: 'posts#create'
+  get 'posts/:id', to: 'posts#show'
+  get 'posts/:id/edit', to: 'post#edit'
 
-  get '/home/top'
-  get '/home/about'
+  get 'home/top'
+  get 'home/about'
   root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
