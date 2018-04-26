4f73dda22750e6f1d33dcb0ae5e4083fa577c291
Naoki Shinagawa <naoki.shina@gmail.com>
2018-04-26 14:33:00 +0900

    add alert message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,17 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to posts_index_path
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to posts_index_path
+    else
+      render("/posts/new")
+    end
   end
 
   def show
@@ -22,12 +27,16 @@ class PostsController < ApplicationController
 
   def update
     @post.content = params[:content]
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      redirect_to("/posts/index")
+    else
+      render("/posts/edit")
+    end
   end
 
   def destroy
     @post.destroy
+    flash[:notice] = "投稿を削除しました"
     redirect_to posts_index_path
   end
 
