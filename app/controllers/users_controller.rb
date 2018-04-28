d8954aaea735d82c85195e133a713a338bf5a258
Naoki Shinagawa <naoki.shina@gmail.com>
2018-04-28 12:07:09 +0900

    add update image for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email], image_name: params[:image_name])
+    @user = User.new(
+      name: params[:name], email: params[:email], image_name: "LGTM.jpg"
+      )
     if @user.save
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to users_index_path
@@ -27,9 +29,15 @@ class UsersController < ApplicationController
   def update
     @user.name = params[:name]
     @user.email = params[:email]
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+
     if @user.save
       flash[:notice] = "ユーザー情報を編集しました"
-      redirect_to("/users/#{@user.id}/edit")
+      redirect_to("/users/#{@user.id}")
     else
       render("/users/edit")
     end
