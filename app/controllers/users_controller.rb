fb315098de1ebc53d55c2e28d652eefa095e80bf
Naoki Shinagawa <naoki.shina@gmail.com>
2018-04-28 16:25:25 +0900

    add ensure correct user for edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, only: [:index, :show, :edit, :update]
   before_action :forbid_login_user, only: [:new, :create, :login_form, :login]
+  before_action :ensure_correct_user, only: [:edit, :update]
   before_action :set_variables, only: [:edit, :update, :show]
   def index
     @users = User.all
@@ -70,5 +71,12 @@ class UsersController < ApplicationController
   private
   def set_variables
     @user = User.find_by(id: params[:id])
+  end
+
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to ("/posts/index")
+    end
   end
 end
