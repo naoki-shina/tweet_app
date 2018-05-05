19d36aeaadea2134c58d91708c47b95b8b6762ae
Naoki Shinagawa <naoki.shina@gmail.com>
2018-05-05 19:36:33 +0900

    add gem n change pass column migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180501104758) do
+ActiveRecord::Schema.define(version: 20180505102631) do
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id", null: false
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20180501104758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
